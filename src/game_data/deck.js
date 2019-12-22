702df801db30d77b61e340ebff63a96d19ff8815
sebek78 <sebasporek@gmail.com>
2019-12-22 12:18:42 +0100

    check the empty deck and shuffle discarded cards

--- src/game_data/deck.js
+++ src/game_data/deck.js
@@ -16,7 +16,7 @@ deckData.forEach(cardData => {
   }
 });
 
-function shuffle(arr) {
+export function shuffle(arr) {
   for (let i = arr.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     const x = arr[i];
